e154a6207372c2c12f77acb1d9a09d7c81e1daef
Nestorke <nestormuuo191@gmail.com>
2024-06-02 16:20:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -14,8 +14,8 @@ global.owner = [
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
+global.mods = ['917605902011','32460248586',254712644756] 
+global.prems = ['917605902011', '32460248586', '919398758484',254712644756]
 global.allowed = ['917605902011', '32460248586', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
